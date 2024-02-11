db3945903d6a6c4e1092619990d5e4f3f7545e9d
Soham Boravke <sohamboravke2004@gmail.com>
2024-02-11 11:07:58 +0530

    Added RewardsValue Class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,27 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue * CONVERSION_RATE;
+    }
+
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue / CONVERSION_RATE;
+        } else {
+            throw new IllegalArgumentException("isMiles must be true");
+        }
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
